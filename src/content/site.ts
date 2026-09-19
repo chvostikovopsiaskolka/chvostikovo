a8994198bcecd7a2b56120fe3c7ff34a39ca0909
chvostikovopsiaskolka <chvostikovo.psiaskolka@gmail.com>
2026-09-19 23:57:25 +0200

    Add dog names to three customer reviews

--- src/content/site.ts
+++ src/content/site.ts
@@ -120,7 +120,7 @@ export const REVIEWS = [
     text: "Náš Max navštevuje škôlku od svojich šteniatkovských čias a bolo to naše najlepšie rozhodnutie. Spoznal kopec kamarátov, na ktorých sa stále teší a vzhľadom na to, že má veľa energie, mu škôlka poskytuje dostatočné vybláznenie a interakcie s ostatnými chlpáčmi. Pre nášho Maxa ako člena rodiny chceme to najlepšie, čo nám Chvostíkovo so svojimi úžasnými majiteľmi poskytuje. Škôlku vrelo odporúčame ako to naj miesto pre každého štvornohého nezbedníka.",
   },
   {
-    name: "Gabriela",
+    name: "Gabriela & Arsi",
     text: "Psia škôlka Chvostíkovo je skvelé miesto, kde bude o vášho psíka veľmi dobre postarané. Personál je úplne super – milí, ochotní a vidno, že psy fakt milujú. Prostredie je čisté, bezpečné a psíci majú kopec priestoru na hranie aj oddych. Odporúčam všetkým, ktorí hľadajú miesto, kde sa o ich psa postarajú ako o vlastného. 🥰🐾",
   },
   {
@@ -128,11 +128,11 @@ export const REVIEWS = [
     text: "Chvostíkovo je výborná škôlka. Náš Bullo sa krásne socializuje, našiel si veľa nových kamarátov. Majitelia sú úžasní ľudia, ktorým môžeme dôverovať. Viem, že lepšiu škôlku sme ani vybrať nemohli. Určite škôlku odporúčame. 😉🐾🐾🐾 A ďakujeme, že môže Bullo chodiť k Vám. 🐾😉",
   },
   {
-    name: "Anita",
+    name: "Anita & Bora",
     text: "Super starostlivosť o psíka :) Milí majitelia, dostatok štvornohých nezbedníkov na vyšantenie sa. Určite odporúčam, nebol problém i s jednorazovou návštevou :) Keby bývame bližšie, v Chvostíkove sme určite častejšie. 👍🏻",
   },
   {
-    name: "Martin",
+    name: "Martin & Monty",
     text: "Profesionálny prístup majiteľov. Ideálne miesto pre psíkov na socializáciu, hranie sa a vybitie energie. Náš pes to tu zbožňuje. Odporúčam 😊",
   },
   {
